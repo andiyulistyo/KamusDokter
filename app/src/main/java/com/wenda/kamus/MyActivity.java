63fbac2961f8b31f6c47b71effc5c2e1a38228c4
souttab <souttab@yahoo.com>
2014-10-09 13:29:44 +0700

    ganti layout

--- app/src/main/java/com/wenda/kamus/MyActivity.java
+++ app/src/main/java/com/wenda/kamus/MyActivity.java
@@ -1,68 +1,23 @@
 package com.wenda.kamus;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.EditText;
 
-public class MyActivity extends FragmentActivity implements ListIstilah.IstilahSelectListener {
+import com.wenda.kamus.adapter.ListIstilahAdapter;
+import com.wenda.kamus.util.DatabaseUtil;
+
+import java.util.List;
+
+public class MyActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        if (findViewById(R.id.fragment_container) != null) {
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            // buat instance dari fragment istilah
-            final ListIstilah listIstilah = new ListIstilah();
-
-            listIstilah.setArguments(getIntent().getExtras());
-
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, listIstilah).commit();
-
-            final EditText editText = (EditText) findViewById(R.id.editText);
-            editText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                    String filter = editText.getText().toString().trim();
-                    listIstilah.updateSetAdapter(filter);
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-        }
+        DatabaseUtil database = new DatabaseUtil(this);
+        List<Kamus> allKamus = database.getAllKamus();
+        setListAdapter(new ListIstilahAdapter(this, allKamus));
     }
 
-    @Override
-    public void onIstilahSelected(int position) {
-        DetailFragment detailFragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.fragment2);
-
-        if (detailFragment != null) {
-            detailFragment.updateData(position);
-        } else {
-            DetailFragment newDetailFragment = new DetailFragment();
-            Bundle bundle = new Bundle();
-            bundle.putInt(DetailFragment.ARG_POSITION, position);
-            newDetailFragment.setArguments(bundle);
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-            transaction.replace(R.id.fragment_container, newDetailFragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
-        }
-    }
 }
--- app/src/main/java/com/wenda/kamus/adapter/ListIstilahAdapter.java
+++ app/src/main/java/com/wenda/kamus/adapter/ListIstilahAdapter.java
@@ -1,30 +1,65 @@
 package com.wenda.kamus.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.wenda.kamus.Kamus;
+import com.wenda.kamus.R;
+
+import java.util.List;
 
 public class ListIstilahAdapter extends BaseAdapter {
 
+    private Context mContext;
+    private LayoutInflater inflater;
+    private List<Kamus> kamusList;
 
+    public ListIstilahAdapter(Context mContext, List<Kamus> kamusList) {
+        this.mContext = mContext;
+        this.kamusList = kamusList;
+        this.inflater = LayoutInflater.from(mContext);
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return kamusList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return kamusList.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+
+        if (view == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.item_list, viewGroup, false);
+            view.setTag(holder);
+
+            holder.textViewIstilah = (TextView) view.findViewById(R.id.textViewIstilah);
+            holder.textViewPenjelasan = (TextView) view.findViewById(R.id.textViewPenjelasan);
+        } else holder = (ViewHolder) view.getTag();
+
+        holder.textViewPenjelasan.setText(kamusList.get(position).getArti());
+        holder.textViewIstilah.setText(kamusList.get(position).getIstilah());
+
+        return view;
     }
+
+    class ViewHolder {
+        TextView textViewIstilah, textViewPenjelasan;
+    }
+
 }
